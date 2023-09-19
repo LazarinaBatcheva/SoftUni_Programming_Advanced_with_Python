753b13895c53b9b61b57365528cc585faecb596b
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-09-19 18:36:19 +0200

    Update 2_expression_evaluator.py

--- Python_Advanced_Exercises/Stacks_Qeues_Tuples_and_Sets_Exercise/2_expression_evaluator.py
+++ Python_Advanced_Exercises/Stacks_Qeues_Tuples_and_Sets_Exercise/2_expression_evaluator.py
@@ -2,7 +2,12 @@ from collections import deque
 
 string_expression = input().split()
 
-operators = "*+-/"
+operators = {
+    "*": lambda f, s: f * s,
+    "+": lambda f, s: f + s,
+    "-": lambda f, s: f - s,
+    "/": lambda f, s: f // s,
+}
 numbers = deque()
 
 for element in string_expression:
@@ -11,20 +16,42 @@ for element in string_expression:
             first_number = numbers.popleft()
             second_number = numbers.popleft()
 
-            result = 0
-
-            if element == "*":
-                result += first_number * second_number
-            elif element == "+":
-                result += first_number + second_number
-            elif element == "/":
-                result += first_number // second_number
-            elif element == "-":
-                result += first_number - second_number
-
-            numbers.appendleft(result)
+            numbers.appendleft(operators[element](first_number, second_number))
 
     else:
         numbers.append(int(element))
 
 print(numbers.pop())
+
+
+
+# from collections import deque
+
+# string_expression = input().split()
+
+# operators = "*+-/"
+# numbers = deque()
+
+# for element in string_expression:
+#     if element in operators:
+#         while len(numbers) > 1:
+#             first_number = numbers.popleft()
+#             second_number = numbers.popleft()
+
+#             result = 0
+
+#             if element == "*":
+#                 result += first_number * second_number
+#             elif element == "+":
+#                 result += first_number + second_number
+#             elif element == "/":
+#                 result += first_number // second_number
+#             elif element == "-":
+#                 result += first_number - second_number
+
+#             numbers.appendleft(result)
+
+#     else:
+#         numbers.append(int(element))
+
+# print(numbers.pop())
