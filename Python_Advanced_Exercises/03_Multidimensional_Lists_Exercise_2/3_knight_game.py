d2f485e00e0c764a580fa46d00618c65277f1c93
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-09-29 20:16:28 +0200

    Add files via upload

---
+++ Python_Advanced_Exercises/03_Multidimensional_Lists_Exercise_2/3_knight_game.py
@@ -0,0 +1,33 @@
+size = int(input())
+matrix = []
+knights = []
+
+for row in range(size):
+    matrix.append(list(input()))
+    for col in range(size):
+        if matrix[row][col] == "K":
+            knights.append([row, col])
+
+removed_knights = 0
+possible_moves = [(-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]
+
+while True:
+    max_hits = 0
+    max_knight = [0, 0]
+    for k_row, k_col in knights:
+        hits = 0
+        for move in possible_moves:
+            new_row, new_col = k_row + move[0], k_col + move[1]
+            if 0 <= new_row < size and 0 <= new_col < size:
+                if matrix[new_row][new_col] == "K":
+                    hits += 1
+        if hits > max_hits:
+            max_hits = hits
+            max_knight = [k_row, k_col]
+    if max_hits == 0:
+        break
+    knights.remove(max_knight)
+    matrix[max_knight[0]][max_knight[1]] = "0"
+    removed_knights += 1
+
+print(removed_knights)
