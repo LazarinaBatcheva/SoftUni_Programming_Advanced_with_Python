8b8ab70f3e6fef4126d472edcf70688ca262b21a
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-10-01 13:58:40 +0200

    Update 3_even_or_odd.py

--- Python_Advanced_Exercises/04_Functions_Advanced_Exercise/3_even_or_odd.py
+++ Python_Advanced_Exercises/04_Functions_Advanced_Exercise/3_even_or_odd.py
@@ -1,12 +1,12 @@
 def even_odd(*args):
-    even = []
-    odd = []
+    even = [int(x) for x in args[:-1] if int(x) % 2 == 0]
+    odd = [int(x) for x in args[:-1] if int(x) % 2 != 0]
 
-    for number in args[:-1]:
-        if int(number) % 2 == 0:
-            even.append(number)
-        else:
-            odd.append(number)
+    # for number in args[:-1]:
+    #     if int(number) % 2 == 0:
+    #         even.append(number)
+    #     else:
+    #         odd.append(number)
 
     if args[-1] == "even":
         return even
