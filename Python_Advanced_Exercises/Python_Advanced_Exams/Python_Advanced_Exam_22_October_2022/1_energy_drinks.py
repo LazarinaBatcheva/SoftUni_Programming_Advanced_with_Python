ff86958dd8a45988f442b67607b70a5bd21880dc
Lazarina-Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-10-18 12:08:18 +0200

    Create 1_energy_drinks.py

---
+++ Python_Advanced_Exercises/Python_Advanced_Exams/Python_Advanced_Exam_22_October_2022/1_energy_drinks.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+MAX_CAFFEINE = 300
+
+milligrams_of_caffeine = [int(x) for x in input().split(', ')]
+energy_drinks = deque(int(x) for x in input().split(', '))
+
+consumed_caffeine = 0
+
+while milligrams_of_caffeine and energy_drinks:
+    current_caffeine = milligrams_of_caffeine.pop()
+    current_energy_drink = energy_drinks.popleft()
+    total_caffeine = current_caffeine * current_energy_drink
+
+    if consumed_caffeine + total_caffeine <= MAX_CAFFEINE:
+        consumed_caffeine += total_caffeine
+
+    else:
+        if consumed_caffeine:
+            consumed_caffeine -= 30
+        energy_drinks.append(current_energy_drink)
+
+if energy_drinks:
+    print(f'Drinks left: {", ".join(map(str, energy_drinks))}')
+else:
+    print("At least Stamat wasn't exceeding the maximum caffeine.")
+
+print(f'Stamat is going to sleep with {consumed_caffeine} mg caffeine.')
