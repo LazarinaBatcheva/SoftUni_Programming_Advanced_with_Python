7b68ddf542048bbe33cb72cb8ab1bcc60ba929d9
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-10-19 21:26:15 +0200

    Update 1_stewards.py

--- Python_Advanced_Exercises/Python_Advanced_Exams/Python_Advanced_Retake_Exam_18_August_2022/1_stewards.py
+++ Python_Advanced_Exercises/Python_Advanced_Exams/Python_Advanced_Retake_Exam_18_August_2022/1_stewards.py
@@ -2,8 +2,7 @@ from collections import deque
 
 
 def calculate_possible_seats(num1, num2):
-    sum_numbers = num1 + num2
-    sum_nums_ascii_chr = chr(sum_numbers)
+    sum_nums_ascii_chr = chr(num1 + num2)
     first_seat = str(num1) + sum_nums_ascii_chr
     second_seat = str(num2) + sum_nums_ascii_chr
 
@@ -20,13 +19,13 @@ total_rotations_made = 0
 while len(seats_matches) < 3 and total_rotations_made < 10:
     first_number = first_nums_sequence[0]
     second_number = second_nums_sequence[-1]
+    total_rotations_made += 1
 
     first_possible_seat, second_possible_seat = calculate_possible_seats(first_number, second_number)
 
     if first_possible_seat not in seats and second_possible_seat not in seats:
         first_nums_sequence.rotate(-1)
         second_nums_sequence.rotate(1)
-        total_rotations_made += 1
         continue
 
     if first_possible_seat in seats:
@@ -39,7 +38,6 @@ while len(seats_matches) < 3 and total_rotations_made < 10:
             
     first_nums_sequence.popleft()
     second_nums_sequence.pop()
-    total_rotations_made += 1
 
 print(f'Seat matches: {", ".join(seats_matches)}')
 
