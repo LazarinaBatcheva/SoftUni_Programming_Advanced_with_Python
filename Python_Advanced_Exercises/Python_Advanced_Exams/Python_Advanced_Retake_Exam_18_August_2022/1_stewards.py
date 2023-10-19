23abd608228f76e95383602c443115d42dd56ba1
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-10-19 21:08:01 +0200

    Update 1_stewards.py

--- Python_Advanced_Exercises/Python_Advanced_Exams/Python_Advanced_Retake_Exam_18_August_2022/1_stewards.py
+++ Python_Advanced_Exercises/Python_Advanced_Exams/Python_Advanced_Retake_Exam_18_August_2022/1_stewards.py
@@ -1,6 +1,15 @@
 from collections import deque
 
 
+def calculate_possible_seats(num1, num2):
+    sum_numbers = num1 + num2
+    sum_nums_ascii_chr = chr(sum_numbers)
+    first_seat = str(num1) + sum_nums_ascii_chr
+    second_seat = str(num2) + sum_nums_ascii_chr
+
+    return first_seat, second_seat
+
+
 seats = list(input().split(', '))
 first_nums_sequence = deque(int(x) for x in input().split(', '))
 second_nums_sequence = deque(int(x) for x in input().split(', '))
@@ -11,11 +20,8 @@ total_rotations_made = 0
 while len(seats_matches) < 3 and total_rotations_made < 10:
     first_number = first_nums_sequence[0]
     second_number = second_nums_sequence[-1]
-    sum_numbers = first_number + second_number
-    sum_nums_ascii_chr = chr(sum_numbers)
 
-    first_possible_seat = str(first_number) + sum_nums_ascii_chr
-    second_possible_seat = str(second_number) + sum_nums_ascii_chr
+    first_possible_seat, second_possible_seat = calculate_possible_seats(first_number, second_number)
 
     if first_possible_seat not in seats and second_possible_seat not in seats:
         first_nums_sequence.rotate(-1)
