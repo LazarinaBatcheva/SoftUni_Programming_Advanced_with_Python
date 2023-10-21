e921ec4bf9ea71860169d85c1d58b9c0fd78a569
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-10-21 16:05:16 +0200

    Add files via upload

---
+++ Python_Advanced_Exams/Python_Advanced_Exam_25_June_2022/3_shopping_cart.py
@@ -0,0 +1,48 @@
+def shopping_cart(*products):
+    meals_limit = {'Soup': 3, 'Pizza': 4, 'Dessert': 2}
+    meals = {'Soup': [], 'Pizza': [], 'Dessert': []}
+    for product in products:
+        if product == 'Stop':
+            break
+        meal, product = product
+        if meal in meals.keys():
+            if product not in meals[meal] and len(meals[meal]) != meals_limit[meal]:
+                meals[meal].append(product)
+
+    result = ''
+
+    if not meals['Soup'] and not meals['Pizza'] and not meals['Dessert']:
+        result += 'No products in the cart!'
+    else:
+        sorted_meals = sorted(meals.items(), key=lambda kvp: (-len(kvp[1]), kvp[0]))
+        for meal_type, added_product in sorted_meals:
+            result += f'{meal_type}:\n'
+            for current_product in sorted(added_product):
+                result += f' - {current_product}\n'
+
+    return result
+
+
+print(shopping_cart(
+    ('Pizza', 'ham'),
+    ('Soup', 'carrots'),
+    ('Pizza', 'cheese'),
+    ('Pizza', 'flour'),
+    ('Dessert', 'milk'),
+    ('Pizza', 'mushrooms'),
+    ('Pizza', 'tomatoes'),
+    'Stop',
+))
+print()
+print(shopping_cart(
+    ('Pizza', 'ham'),
+    ('Dessert', 'milk'),
+    ('Pizza', 'ham'),
+    'Stop',
+))
+print()
+print(shopping_cart(
+    'Stop',
+    ('Pizza', 'ham'),
+    ('Pizza', 'mushrooms'),
+))
