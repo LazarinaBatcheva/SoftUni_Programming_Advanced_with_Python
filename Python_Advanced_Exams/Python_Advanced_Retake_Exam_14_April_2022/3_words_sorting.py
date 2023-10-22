efd1654a0565b9dac8fb6a658c27338349ff32d2
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-10-22 10:40:34 +0200

    Add files via upload

---
+++ Python_Advanced_Exams/Python_Advanced_Retake_Exam_14_April_2022/3_words_sorting.py
@@ -0,0 +1,35 @@
+def words_sorting(*words: str):
+    words_dict = {}
+    total_sum = 0
+
+    for word in words:
+        words_dict[word] = sum(ord(letter) for letter in word)
+        total_sum += words_dict[word]
+
+    if total_sum % 2 == 0:
+        sorted_dict = sorted(words_dict.items(), key=lambda kvp: kvp[0])
+    else:
+        sorted_dict = sorted(words_dict.items(), key=lambda kvp: -kvp[1])
+
+    return '\n'.join(f'{current_word} - {current_sum}' for current_word, current_sum in sorted_dict)
+
+
+print(
+    words_sorting(
+        'escape',
+        'charm',
+        'mythology'
+  ))
+print()
+print(
+    words_sorting(
+        'escape',
+        'charm',
+        'eye'
+  ))
+print()
+print(
+    words_sorting(
+        'cacophony',
+        'accolade'
+  ))
